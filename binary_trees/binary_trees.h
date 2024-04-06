58bb1f198efcbcc4c31de677bfb474fb48145b2a
El Mehdi <azghoulmehdi899@gmail.com>
2024-04-06 15:49:47 +0200

    task 0 fix2

--- binary_trees/binary_trees.h
+++ binary_trees/binary_trees.h
@@ -1,3 +1,5 @@
+#ifndef _BINARY_TREE_
+#define _BINARY_TREE_
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -15,3 +17,8 @@ struct binary_tree_s
 };
 
 typedef struct binary_tree_s binary_tree_t;
+
+void binary_tree_print(const binary_tree_t *);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif
